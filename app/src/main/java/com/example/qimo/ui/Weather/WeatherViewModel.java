f0d53cfbfc36e422e9f76bd02dde7239cd91a055
LiMengyang-6226 <645325755@qq.com>
2020-12-25 22:36:41 +0800

    first commit

---
+++ app/src/main/java/com/example/qimo/ui/Weather/WeatherViewModel.java
@@ -0,0 +1,7 @@
+package com.example.qimo.ui.Weather;
+
+import androidx.lifecycle.ViewModel;
+
+public class WeatherViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/qimo/ui/watch/WatchViewModel.java
@@ -0,0 +1,7 @@
+package com.example.qimo.ui.watch;
+
+import androidx.lifecycle.ViewModel;
+
+public class WatchViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
